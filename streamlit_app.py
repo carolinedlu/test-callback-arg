1e7adc7cd9416c80c03497feeb37f108e1736f95
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2022-06-01 11:59:34 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,5 @@ def callback_fxn(my_text_input):
 
 my_form = st.form("my-form")
 st.write("this is my form")
-my_form.my_text_input = st.text_input("write something")
+my_text_input = my_form.text_input("write something")
 my_form.form_submit_button(label='submit stuff', on_click=callback_fxn, args=(my_text_input, ))
