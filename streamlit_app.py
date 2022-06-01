fd39a5a3444ed93ad8879c9ae3ee43535c74a343
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2022-06-01 11:47:59 -0400

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+with callback_fxn(my_text_input):
+  st.write("here's the val:")
+  st.write(my_text_input)
+
+with st.form():
+  st.write("this is my form")
+  my_text_input = st.text_input("write something")
+  submit = st.form_submit_button(label='submit stuff', on_click=callback_fxn(), args=(my_text_input, ))
